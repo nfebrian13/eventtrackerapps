36a533cb4f9d8573fd64e66f660d9f94d0a6a99f
nfebrian13 <acer@DESKTOP-U7I9Q7E>
2019-09-28 20:44:06 +0700

    adding static configuration for pdf and css

--- EventTracker/src/main/java/com/nana/WebConfig.java
+++ EventTracker/src/main/java/com/nana/WebConfig.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.nana")
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
 	@Bean
 	public InternalResourceViewResolver getInternalResourceViewResolver() {
@@ -17,5 +19,11 @@ public class WebConfig {
 		resolver.setPrefix("/WEB-INF/jsp/");
 		resolver.setSuffix(".jsp");
 		return resolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/pdfs/**").addResourceLocations("/WEB-INF/pdf/");
+		registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/css/");
 	}
 }
