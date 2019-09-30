90ce4ec06730517335b0b50e97333a5aca1e2ab5
nfebrian13 <acer@DESKTOP-U7I9Q7E>
2019-09-30 16:33:40 +0700

    event controller

--- EventTracker/src/main/java/com/nana/controller/EventController.java
+++ EventTracker/src/main/java/com/nana/controller/EventController.java
@@ -2,6 +2,7 @@ package com.nana.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,6 +16,13 @@ public class EventController {
 		Event event = new Event();
 		event.setName("Java User Group");
 		model.addAttribute("event", event);
+		return "event";
+	}
+
+	@RequestMapping(value = "/event", method = RequestMethod.POST)
+	public String processEvent(@ModelAttribute("event") Event event) {
+		System.out.println(event);
+		
 		return "event";
 	}
 }
